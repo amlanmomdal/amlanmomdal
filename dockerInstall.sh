897a193322819cc052c825f588f26f23b1b4b236
amlanmomdal <43263214+amlanmomdal@users.noreply.github.com>
2023-02-07 16:18:54 +0530

    Add files via upload

---
+++ dockerInstall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sleep 2
+
+docker pull amlanmondal/test-lala
